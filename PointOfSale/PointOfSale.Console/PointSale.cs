c5b4b673915596eeaea526840daae91a6ebbd06b
cvigueras <carlosj.vigueras@gmail.com>
2023-04-04 13:56:33 +0200

    Feat: return error bar code not found when bar code is 99999

--- PointOfSale/PointOfSale.Console/PointSale.cs
+++ PointOfSale/PointOfSale.Console/PointSale.cs
@@ -8,6 +8,12 @@ public class PointSale
         {
             return "Error: empty barcode";
         }
+
+        if (barCode.Value == "99999")
+        {
+            return "Error: barcode not found";
+        }
+
         return barCode.GetPrice().ToString();
 
     }
