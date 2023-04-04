852192e3b599e9a2b298526f5d283217b368d83f
cvigueras <carlosj.vigueras@gmail.com>
2023-04-04 13:03:36 +0200

    Chore: Init project

---
+++ PointOfSale/PointOfSale.Test/PointOfSaleShould.cs
@@ -0,0 +1,16 @@
+namespace PointOfSale.Test
+{
+    public class PointOfSaleShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
