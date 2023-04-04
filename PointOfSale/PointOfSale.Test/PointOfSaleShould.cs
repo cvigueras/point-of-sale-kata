b1cc646d7779a0bc4e24ca0ea31d39e0148d5266
cvigueras <carlosj.vigueras@gmail.com>
2023-04-04 13:29:11 +0200

    Test: return_cost_when_bar_code_exist

--- PointOfSale/PointOfSale.Test/PointOfSaleShould.cs
+++ PointOfSale/PointOfSale.Test/PointOfSaleShould.cs
@@ -25,5 +25,13 @@ namespace PointOfSale.Test
 
             result.Should().Be("Error: empty barcode");
         }
+
+        [Test]
+        public void return_cost_when_bar_code_exist()
+        {
+            var result = PointSale.GetTotal("12345");
+
+            result.Should().Be("$7.25");
+        }
     }
 }
